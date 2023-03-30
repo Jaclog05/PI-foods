1f4e55294788b218ca9b9cb0f89565a5284a21fd
Jaider <jaidercueto.1010@gmail.com>
2023-03-30 16:42:25 -0500

    closing button added to cards with functionality

--- client/src/components/RecipeCard/RecipeCard.jsx
+++ client/src/components/RecipeCard/RecipeCard.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styles from './RecipeCard.module.css';
 import defaultNewRecipe from '../../images/defaultNewRecipe.png'
+import { useDispatch } from 'react-redux';
+import * as actions from "../../redux/actions";
 
 
-function GameCard({name, image, diet, healthScore}) {
+function GameCard({id, name, image, diet, healthScore}) {
+
+  const dispatch = useDispatch()
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    dispatch(actions.deleteRecipe(id))
+  } 
 
   return (
     <div className={styles.wrapper}>
         <div className={styles.upper}>
             <div>{healthScore}</div>
             <h4 style={{margin: 4}}>{name}</h4>
+            {typeof id !== 'number' && <button onClick={handleClick}>x</button>}
         </div>
         <img className={styles.images} src={image ? image : defaultNewRecipe} alt={name} width="210" height="140"/>
         <div className={styles.genresDiv}>
