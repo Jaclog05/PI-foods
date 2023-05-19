345ef544dcc99a3d8ea89c13df7de40d25566af7
Jaider <jaidercueto.1010@gmail.com>
2023-05-19 11:46:03 -0500

    importing and using 'createRecipes' controller in dietRoutes

--- api/src/routes/dietRoutes.js
+++ api/src/routes/dietRoutes.js
@@ -1,22 +1,9 @@
 let express = require('express')
 let { Diet } = require('../db')
+const { createDiets } = require('../controllers')
 
 const dietRouter = express.Router()
 
-const dietsObj = {
-    "gluten free": false,
-    "ketogenic": false,
-    "vegetarian": false,
-    "lacto-vegetarian": false,
-    "ovo-vegetarian": false,
-    "vegan": false,
-    "pescetarian": false,
-    "paleo": false,
-    "paleolithic": false,
-    "primal": false,
-    "low FODMAP": false,
-    "whole30": false
-}
 
 dietRouter.get('/', async (req, res) => {
 
@@ -24,14 +11,8 @@ dietRouter.get('/', async (req, res) => {
 
     if (count === 0 && !rows.length) {
 
-        let formatForDb = Object.keys(dietsObj).map(item => {
-            return {
-                name: item
-            }
-        })
-    
-        const dietsOnDb = await Diet.bulkCreate(formatForDb)
-        
+        const formatForDb = createDiets()
+        const dietsOnDb = await Diet.bulkCreate(formatForDb)   
         return res.status(201).json(dietsOnDb)
 
     }else{
